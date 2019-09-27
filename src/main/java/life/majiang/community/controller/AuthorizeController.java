57ff4b488f723b1f3f6bff7f298f3d27e1626d01
lbd5788 <1069963250@qq.com>
2019-09-27 11:45:53 +0800

    添加连接h2并写入h2

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package life.majiang.community.controller;
 
 import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GithupUser;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
 import life.majiang.community.provider.GithupProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -19,10 +22,16 @@ public class AuthorizeController {
 
     @Value("${githup.Client_id}")
     private String clientId;
+
     @Value("${githup.Redirect_uri}")
     private String RedirectUri;
+
     @Value("${githup.Client_secret}")
     private String ClientSecret;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -34,10 +43,18 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(ClientSecret);
         String accessToken = githupProvider.getAccessToken(accessTokenDTO);
-        GithupUser user = githupProvider.getUser(accessToken);
-       if(user!=null){
+        GithupUser githupuser = githupProvider.getUser(accessToken);
+       if(githupuser!=null){
         //登录成功，写session和cookies
-            request.getSession().setAttribute("user",user);
+           User user = new User();
+           user.setToken(UUID.randomUUID().toString());
+           user.setAccounId(String.valueOf(githupuser.getId()));
+           user.setName(githupuser.getName());
+           user.setGmtCreate(System.currentTimeMillis());
+           user.setGmtModified(user.getGmtCreate());
+           userMapper.insert(user);
+
+            request.getSession().setAttribute("user",githupuser);
             return "redirect:/";
 
        }else{
